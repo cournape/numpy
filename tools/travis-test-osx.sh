d7862526855c7e9b2a9ea95048b85d49abfee1b2
David Cournapeau <cournape@gmail.com>
2017-07-15 21:53:32 +0100

    fix typo in pushd.

--- tools/travis-test-osx.sh
+++ tools/travis-test-osx.sh
@@ -5,10 +5,11 @@ python setup.py bdist_wheel
 python -m virtualenv venv-for-wheel
 . venv-for-wheel/bin/activate
 
-pushd dir
+pushd dist
 pip install --pre --no-index --upgrade --find-links=. numpy
 popd
 
+mkdir -p empty
 pushd empty
 python ../tools/test-installed-numpy.py
 popd
