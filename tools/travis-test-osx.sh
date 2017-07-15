547942900540f946a3da6b81d9644137960339f8
David Cournapeau <cournape@gmail.com>
2017-07-15 21:35:22 +0100

    fix missing arg to clean_code function.

--- tools/travis-test-osx.sh
+++ tools/travis-test-osx.sh
@@ -3,7 +3,7 @@ export NPY_NUM_BUILD_JOBS=2
 source multibuild/common_utils.sh
 source multibuild/travis_steps.sh
 
-clean_code numpy
+clean_code numpy "${TRAVIS_COMMIT}"
 build_wheel numpy x86_64
 
 install_run x86_64
