e1276e56784a412018b83a8627b76e98724af543
David Cournapeau <cournape@gmail.com>
2017-07-16 10:22:17 +0100

    bug: use virtualenv instead of venv to get pip/setuptools.

--- tools/travis-test-osx.sh
+++ tools/travis-test-osx.sh
@@ -8,12 +8,17 @@ python -V
 python -c "import sys; print(sys.exec_prefix)"
 
 pip install cython nose pytz
+export CFLAGS="-Wno-sign-compare -x86_64 -O0"
+export CXXFLAGS="-Wno-sign-compare -x86_64 -O0"
+export CC=clang
+export CXX=clang++
 python setup.py bdist_wheel
 
-python -m venv venv-for-wheel
+virtualenv venv-for-wheel
 . venv-for-wheel/bin/activate
 python -V
 python -c "import sys; print(sys.exec_prefix)"
+pip install nose pytz
 
 pushd dist
 pip install --pre --no-index --upgrade --find-links=. numpy
